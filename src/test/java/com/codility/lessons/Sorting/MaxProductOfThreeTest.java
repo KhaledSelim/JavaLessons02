78e442354b751ff342c3591c9efbcd1faf89f2fc
Khaled Selim <khaledselim3010@hotmail.com>
2024-12-10 20:11:14 +0200

    Update MaxProductOfThreeTest.java

--- src/test/java/com/codility/lessons/Sorting/MaxProductOfThreeTest.java
+++ src/test/java/com/codility/lessons/Sorting/MaxProductOfThreeTest.java
@@ -16,10 +16,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import com.codility.lessons.CountingElements.PermutationCheckTest;
 
 /**
- * https://app.codility.com/demo/results/trainingJXXKP9-4EM/
- * 
- * @author Sunil
  *
+ * @author khaled.selim
  */
 @RunWith(SpringRunner.class)
 public class MaxProductOfThreeTest {
